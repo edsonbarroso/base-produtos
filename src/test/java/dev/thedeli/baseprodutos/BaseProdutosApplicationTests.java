a6744ff4e28232774e2116c3f9e8977dfa188ead
edsonbarroso <edsonbarroso.dev@gmail.com>
2024-05-09 21:36:12 -0300

    upload Application e HomeController

---
+++ src/test/java/dev/thedeli/baseprodutos/BaseProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.thedeli.baseprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
